63505595ee5888020d2ce4289f15427a370997cd
Mattias <drachbar@gmail.com>
2025-02-05 22:28:40 +0100

    Lägg till strömmande chat istället

--- src/main/java/se/drachbar/Main.java
+++ src/main/java/se/drachbar/Main.java
@@ -1,6 +1,13 @@
 package se.drachbar;
 
-import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.StreamingChatLanguageModel;
+import dev.langchain4j.model.chat.request.ChatRequest;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.model.chat.response.StreamingChatResponseHandler;
+import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
+
+import java.util.List;
 
 import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
 
@@ -8,12 +15,30 @@ public class Main {
     public static void main(String[] args) {
         String apiKey = System.getenv("OPENAI_API_KEY");
 
-        OpenAiChatModel model = OpenAiChatModel.builder()
+        StreamingChatLanguageModel chatModel = OpenAiStreamingChatModel.builder()
                 .apiKey(apiKey)
                 .modelName(GPT_4_O_MINI)
                 .build();
 
-        String answer = model.generate("Hi, whats up?");
-        System.out.println(answer);
+        chatModel.chat(ChatRequest.builder()
+                .messages(
+                        List.of(new UserMessage("Tell me a joke about Java"))
+                ).build(), new StreamingChatResponseHandler() {
+            @Override
+            public void onPartialResponse(String s) {
+                System.out.print(s);
+            }
+
+            @Override
+            public void onCompleteResponse(ChatResponse chatResponse) {
+                System.out.println();
+                System.out.println(chatResponse.aiMessage().text());
+            }
+
+            @Override
+            public void onError(Throwable error) {
+                error.printStackTrace();
+            }
+        });
     }
 }
